9f63449c813ce5f9a47a67099e062976bde483a7
chungyo <kimchungy98@gachon.ac.kr>
2025-04-28 22:40:13 +0900

    :recycle: refactor: getUser.js url 수정

--- src/apis/user/getUser.js
+++ src/apis/user/getUser.js
@@ -1,6 +1,6 @@
 import axiosInstance from "../axiosInstance";
 
 export const getUser = async () => {
-    const { data } = await axiosInstance.get("/api/user/me");
+    const { data } = await axiosInstance.get("/api/users/me");
     return data;
 };
